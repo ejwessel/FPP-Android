69b44f3916f14660a4256c9b6b20800e46b4e334
Ethan Wessel <ejwessel@gmail.com>
2014-02-27 09:21:19 -0800

    got transitioning between activities down.

--- src/com/example/fundamentalplasmaparameters/MainActivity.java
+++ src/com/example/fundamentalplasmaparameters/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.fundamentalplasmaparameters;
 
 import java.util.ArrayList;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
@@ -18,10 +24,22 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		this.setTitle(R.string.label_name); //set the Top title bar to the
 		
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		
 		initList();
 		ListView optionList = (ListView) findViewById(R.id.listView);
 		ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nameList);
 		optionList.setAdapter(adapter); //populates list
+		optionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			     public void onItemClick(AdapterView<?> parentAdapter, View view, int position, long id) {
+			         TextView clickedView = (TextView) view;
+			         String textClicked = clickedView.getText().toString();
+			         
+			         startActivity(new Intent(MainActivity.this, MainActivity.class));
+			         overridePendingTransition(R.anim.right_slide_in, R.anim.left_slide_out);
+			     }
+		});
 	}
 
 	@Override
@@ -29,8 +47,8 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-
+	}	
+	
 	private void initList(){
 		nameList.add("Frequencies");
 		nameList.add("Lengths");
@@ -39,4 +57,16 @@ public class MainActivity extends Activity {
 		nameList.add("Miscellaneous");
 		nameList.add("About");
 	}	
+	
+	@Override
+	public void onBackPressed() {
+	    super.onBackPressed();
+	    overridePendingTransition(R.anim.left_slide_in, R.anim.right_slide_out);
+	}
+	@Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){       
+        startActivity(new Intent(MainActivity.this,MainActivity.class));
+        onBackPressed();
+        return true;
+    }
 }
