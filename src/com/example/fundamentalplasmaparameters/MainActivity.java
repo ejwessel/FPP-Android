440505742f8d7f470c0f10fb417eb51235018c1f
Ethan Wessel <ejwessel@gmail.com>
2014-02-27 05:18:32 -0800

    First Commit.

---
+++ src/com/example/fundamentalplasmaparameters/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.fundamentalplasmaparameters;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		this.setTitle(R.string.label_name); //set the Top title bar to the 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
